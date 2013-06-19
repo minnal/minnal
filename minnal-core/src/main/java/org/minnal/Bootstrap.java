23522f0fd1e9d42a7bd34048edf64cf27d94da24
ganeshs <ganeshs@flipkart.com>
2013-06-19 16:46:02 +0530

    Fixed #17 Server shutdown support must be provided.

--- minnal-core/src/main/java/org/minnal/Bootstrap.java
+++ minnal-core/src/main/java/org/minnal/Bootstrap.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * <p>Bootstrap is the entry point to minnal and is the main class. The main method loads the container, initializes and starts the container. 
+ * It also registers a shutdown hook for gracefully shutting down the container. To stop the minnal server, use SIGTERM the running process</p> 
+ *  
  * @author ganeshs
  *
  */
@@ -16,15 +19,24 @@ public class Bootstrap {
 
 	public static void main(String[] args) {
 		try {
-			Class<?> containerClass = Thread.currentThread().getContextClassLoader().loadClass("org.minnal.core.Container");
-			Object container = null;
-			if (args != null && args.length > 0) {
-				container = containerClass.getConstructor(String.class).newInstance(args[0]);
-			} else {
-				container = containerClass.newInstance();
-			}
+			final Class<?> containerClass = Thread.currentThread().getContextClassLoader().loadClass("org.minnal.core.Container");
+			final Object container = args != null && args.length > 0 ? 
+					containerClass.getConstructor(String.class).newInstance(args[0]) : 
+					containerClass.newInstance();
+
 			containerClass.getMethod("init").invoke(container);
 			containerClass.getMethod("start").invoke(container);
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				@Override
+				public void run() {
+					try {
+						logger.info("Shutdown hook triggered. Stopping the container");
+						containerClass.getMethod("stop").invoke(container);
+					} catch (Exception e) {
+						logger.error("Failed while stopping the container", e);
+					}
+				}
+			});
 		} catch (Exception e) {
 			logger.error("Failed while starting the container", e);
 			System.exit(1);
