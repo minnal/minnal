69d6de8f9e5c34362253e14dd8f892438679785c
ganeshs <ganeshs@flipkart.com>
2013-07-31 12:05:48 +0530

    Added bean population utility to test module

--- minnal-test/src/main/java/org/minnal/core/resource/BaseResourceTest.java
+++ minnal-test/src/main/java/org/minnal/core/resource/BaseResourceTest.java
@@ -3,10 +3,15 @@
  */
 package org.minnal.core.resource;
 
+import java.io.ByteArrayOutputStream;
+import java.lang.annotation.Annotation;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBufferInputStream;
 import org.jboss.netty.buffer.ChannelBufferOutputStream;
 import org.jboss.netty.buffer.ChannelBuffers;
 import org.jboss.netty.handler.codec.http.DefaultHttpRequest;
@@ -17,6 +22,7 @@ import org.jboss.netty.handler.codec.http.HttpRequest;
 import org.jboss.netty.handler.codec.http.HttpResponseStatus;
 import org.jboss.netty.handler.codec.http.HttpVersion;
 import org.minnal.core.Container;
+import org.minnal.core.MinnalException;
 import org.minnal.core.Response;
 import org.minnal.core.Router;
 import org.minnal.core.serializer.Serializer;
@@ -30,7 +36,13 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import com.google.common.base.Charsets;
 import com.google.common.collect.Maps;
+import com.google.common.io.ByteStreams;
 import com.google.common.net.MediaType;
 
 /**
@@ -40,6 +52,8 @@ import com.google.common.net.MediaType;
 public abstract class BaseResourceTest {
 	
 	private static final Logger logger = LoggerFactory.getLogger(BaseResourceTest.class);
+	
+	private static PodamFactoryImpl podamFactory; 
 	
 	static {
 		Class<?> clazz = null;
@@ -57,6 +71,16 @@ public abstract class BaseResourceTest {
 				throw new Error("Failed while loading the activejpa agent");
 			}
 		}
+		
+		List<Class<? extends Annotation>> excludeAnnotations = new ArrayList<Class<? extends Annotation>>();
+		excludeAnnotations.add(JsonIgnore.class);
+		try {
+			excludeAnnotations.add((Class)ClassUtil.findClass("javax.persistence.GeneratedValue"));
+		} catch (ClassNotFoundException e) {
+			// ignore
+		}
+		podamFactory = new PodamFactoryImpl();
+		podamFactory.setExcludeAnnotations(excludeAnnotations);
 	}
 	
 	private Router router;
@@ -96,6 +120,16 @@ public abstract class BaseResourceTest {
 	
 	protected void route(MessageContext context) {
 		router.route(context);
+	}
+	
+	protected ServerRequest request(String uri, HttpMethod method, ChannelBuffer content) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		try {
+			ByteStreams.copy(new ChannelBufferInputStream(content), bos);
+			return request(uri, method, bos.toString(Charsets.UTF_8.name()), MediaType.JSON_UTF_8);
+		} catch (Exception e) {
+			throw new MinnalException(e);
+		}
 	}
 	
 	protected ServerRequest request(String uri, HttpMethod method, String content) {
@@ -138,4 +172,7 @@ public abstract class BaseResourceTest {
 		return response;
 	}
 	
+	protected <T> T createDomain(Class<T> clazz) {
+		return podamFactory.manufacturePojo(clazz);
+	}
 }
