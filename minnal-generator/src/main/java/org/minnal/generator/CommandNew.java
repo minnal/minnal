0e505d9bbc1c8a38288ca312ca474b89320436d2
ganeshs <ganeshs@flipkart.com>
2013-11-11 17:51:14 +0530

    Incremented the minnal version in generator

--- minnal-generator/src/main/java/org/minnal/generator/CommandNew.java
+++ minnal-generator/src/main/java/org/minnal/generator/CommandNew.java
@@ -35,7 +35,7 @@ public class CommandNew implements Command {
 	private String baseDir = System.getProperty("user.dir");
 	
 	@Parameter(names = "-version", description = "The minnal version to use")
-	private String version = "1.0.7";
+	private String version = "1.1.0";
 
 	/**
 	 * @return the projectName
