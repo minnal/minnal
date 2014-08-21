2dff8b4817b417122273e2db15c92de72494f0a8
Shashank Gautam <shashank.gautam@hightail.com>
2014-08-21 19:58:14 +0530

    baseResourceTest and baseJPATest

---
+++ minnal-jaxrs-test/src/main/java/org/minnal/jaxrs/test/baseTest/BaseJPAResourceTest.java
@@ -0,0 +1,55 @@
+package org.minnal.jaxrs.test.baseTest;
+
+import org.activejpa.enhancer.ActiveJpaAgentLoader;
+import org.activejpa.jpa.JPA;
+import org.activejpa.jpa.JPAContext;
+import org.testng.IObjectFactory;
+import org.testng.ITestContext;
+import org.testng.annotations.ObjectFactory;
+import org.testng.internal.ObjectFactoryImpl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public abstract class BaseJPAResourceTest extends BaseResourceTest{
+
+    /**
+     * Note: Kind of hack to ensure that ActiveJPAAgent instruments all the models before they are loaded.
+     *
+     * @param context
+     * @return
+     * @throws Exception
+     */
+    @ObjectFactory
+    public IObjectFactory getObjectFactory(ITestContext context) throws Exception {
+        ActiveJpaAgentLoader.instance().loadAgent();
+        return new ObjectFactoryImpl();
+    }
+
+    protected void setup() {
+        super.setup();
+        if (! disableForeignKeyChecks()) {
+            return;
+        }
+
+        JPAContext context = JPA.instance.getDefaultConfig().getContext();
+        context.beginTxn();
+        try {
+            EntityManager manager = context.getEntityManager();
+            Query query = manager.createNativeQuery("SET DATABASE REFERENTIAL INTEGRITY FALSE");
+            query.executeUpdate();
+        } finally {
+            context.closeTxn(false);
+        }
+    }
+
+    /**
+     * Override this method if you don't want to disable foreign key checks
+     *
+     * @return
+     */
+    protected boolean disableForeignKeyChecks() {
+        return true;
+    }
+
+}
